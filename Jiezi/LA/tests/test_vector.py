c37830ef71436544776007b4b4fa9352d2592c0a
Chen Song <chen@song>
2021-12-18 23:29:18 +0100

    put test_vector into unittest framework

--- Jiezi/LA/tests/test_vector.py
+++ Jiezi/LA/tests/test_vector.py
@@ -8,41 +8,97 @@
 
 import sys
 import numpy as np
+import unittest
 
 sys.path.append("../../../")
 from Jiezi.LA.vector_numpy import vector_numpy
 
-# test init
-vec = vector_numpy(3)
-assert (vec.get_value() == np.array([0 + 0j, 0 + 0j, 0 + 0j])).all(), "init is wrong"
-# test get_size
-assert vec.get_size() == (3,), "get_size is wrong"
-# test set_value
-vec.set_value(0, 1 + 3j)
-vec.set_value(1, 2 + 2j)
-assert (vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "set_value is wrong"
-# test get_value
-assert vec.get_value(2) == 0 + 0j, "get_value(arg1) is wrong"
-assert (vec.get_value(0, 2) == np.array([1 + 3j, 2 + 2j])).all(), "get_value(arg1, arg2) is wrong"
-# test imaginary
-assert (vec.imaginary().get_value() == np.array([3, 2, 0])).all(), "imaginary is wrong"
-assert (vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed"
-# test real
-assert (vec.real().get_value() == np.array([1, 2, 0])).all(), "real is wrong"
-assert (vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed"
-# test transpose
-assert (vec.trans().get_value() == np.array([[1 + 3j], [2 + 2j], [0 + 0j]])).all(), "transpose is wrong"
-assert (vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed"
-# test conjugate
-assert (vec.conjugate().get_value() == np.array([1 - 3j, 2 - 2j, 0 - 0j])).all(), "conjugate is wrong"
-assert (vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed"
-# test dagger
-assert (vec.dagger().get_value() == np.array([[1 - 3j], [2 - 2j], [0 - 0j]])).all(), "dagger is wrong"
-assert (vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed"
-# test negative
-assert (vec.nega().get_value() == np.array([-1 - 3j, -2 - 2j, 0])).all(), "negative is wrong"
-assert (vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed"
-# test copy
-src = np.array([1. + 2.j, 2. + 3.j, 2])
-vec.copy(src)
-assert (vec.get_value() == np.array([1. + 2.j, 2. + 3.j, 2])).all(), "copy is wrong"
+class TestVector(unittest.TestCase):
+    vec = vector_numpy(3)
+    
+    def test(self):
+        self.init()
+        self.get_size()
+        self.set_value()
+        self.get_value()
+        self.imaginary()
+        self.real()
+        self.transpose()
+        self.conjugate()
+        self.dagger()
+        self.negative()
+        
+    def init(self):
+        self.assertTrue((TestVector.vec.get_value() == \
+        np.array([0 + 0j, 0 + 0j, 0 + 0j])).all(), \
+        "init is wrong")
+
+    def get_size(self):
+        self.assertEqual(TestVector.vec.get_size(), (3,), "get_size is wrong")
+
+    def set_value(self):
+        TestVector.vec.set_value(0, 1 + 3j)
+        TestVector.vec.set_value(1, 2 + 2j)
+        self.assertTrue((TestVector.vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), \
+        "set_value is wrong")
+
+    def get_value(self):
+        self.assertEqual(TestVector.vec.get_value(2), 0 + 0j, \
+        "get_value is wrong")
+
+        self.assertTrue((TestVector.vec.get_value(0, 2) == np.array([1 + 3j, 2 + 2j])).all(), \
+        "get_value is wrong")
+        
+    def imaginary(self):
+        self.assertTrue((TestVector.vec.imaginary().get_value() == np.array([3, 2, 0])).all(), \
+        "imaginary is wrong")
+
+        self.assertTrue((TestVector.vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), \
+        "original value can not be changed")
+
+    def real(self):
+        self.assertTrue((TestVector.vec.real().get_value() == \
+        np.array([1, 2, 0])).all(), "real is wrong")
+
+        self.assertTrue((TestVector.vec.get_value() == \
+        np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), \
+        "original value can not be changed")
+
+    def transpose(self):
+        self.assertTrue((TestVector.vec.trans().get_value() == \
+        np.array([[1 + 3j], [2 + 2j], [0 + 0j]])).all(), "transpose is wrong")
+
+        self.assertTrue((TestVector.vec.get_value() == np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), \
+        "original value can not be changed")
+
+    def conjugate(self):
+        self.assertTrue((TestVector.vec.conjugate().get_value() == \
+        np.array([1 - 3j, 2 - 2j, 0 - 0j])).all(), "conjugate is wrong")
+
+        self.assertTrue((TestVector.vec.get_value() == \
+        np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed")
+        
+    def dagger(self):
+        self.assertTrue((TestVector.vec.dagger().get_value() == \
+        np.array([[1 - 3j], [2 - 2j], [0 - 0j]])).all(), "dagger is wrong")
+
+        self.assertTrue((TestVector.vec.get_value() == \
+        np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed")
+
+    def negative(self):
+        self.assertTrue((TestVector.vec.nega().get_value() == \
+        np.array([-1 - 3j, -2 - 2j, 0])).all(), "negative is wrong")
+
+        self.assertTrue((TestVector.vec.get_value() == \
+        np.array([1 + 3j, 2 + 2j, 0 + 0j])).all(), "original value can not be changed")
+
+    def copy(self):
+        src = np.array([1. + 2.j, 2. + 3.j, 2])
+        TestVector.vec.copy(src)
+        self.assertTrue((TestVector.vec.get_value() == np.array([1. + 2.j, 2. + 3.j, 2])).all(), "copy is wrong")
+
+        
+if __name__ == "__main__":
+    unittest.main()
+
+
